131b4647c9428da5c6f34f71cd5f630bceca7e6c
luisescxx <luis.fernando@landapp.com.br>
2023-06-13 12:52:34 -0300

    chore: add providers to show favorite icon

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -26,13 +26,16 @@ class MealDetailsScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final favoriteMeals = ref.watch(favoriteMealsProvider);
+    final isFavorite = favoriteMeals.contains(meal);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
         actions: [
           IconButton(
             onPressed: () => _showInfoMessage(ref, context),
-            icon: const Icon(Icons.star),
+            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
           ),
         ],
       ),
